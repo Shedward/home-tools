6c5c64017415eba635cbbd31410fd0f9f4d947c0
Vlad Maltsev <shedwardx@gmail.com>
2024-04-03 13:49:31 +0300

    Revert root change

--- Scripts/MakeReleasePackage.sh
+++ Scripts/MakeReleasePackage.sh
@@ -3,16 +3,14 @@
 BUILD_PATH=".build/release"
 
 PACKAGE_PATH="${BUILD_PATH}/Package"
-HOME_TOOLS="${PACKAGE_PATH}/HomeTools"
 
 rm -rf "${PACKAGE_PATH}"
 mkdir "${PACKAGE_PATH}"
-mkdir "${HOME_TOOLS}"
 
-cp -r "Resources" "${HOME_TOOLS}"
-cp -r "Public" "${HOME_TOOLS}"
-cp -r "${BUILD_PATH}/HomeTools" "${HOME_TOOLS}"
-cp -r "Libs" "${HOME_TOOLS}/lib"
+cp -r "Resources" "${PACKAGE_PATH}"
+cp -r "Public" "${PACKAGE_PATH}"
+cp -r "${BUILD_PATH}/HomeTools" "${PACKAGE_PATH}"
+cp -r "Libs" "${PACKAGE_PATH}/lib"
 
 tar -czvf "HomeTools.tar.gz" -C "${PACKAGE_PATH}" .
 
