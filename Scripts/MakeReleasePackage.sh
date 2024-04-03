75115aebaace27876c6878f365340d0982468649
Vlad Maltsev <shedwardx@gmail.com>
2024-04-03 12:24:28 +0300

    Adding missing dynamic libs to package

--- Scripts/MakeReleasePackage.sh
+++ Scripts/MakeReleasePackage.sh
@@ -10,6 +10,7 @@ mkdir "${PACKAGE_PATH}"
 cp -r "Resources" "${PACKAGE_PATH}"
 cp -r "Public" "${PACKAGE_PATH}"
 cp -r "${BUILD_PATH}/HomeTools" "${PACKAGE_PATH}"
+cp -r "Libs" "${PACKAGE_PATH}/lib"
 
 tar -czvf "HomeTools.tar.gz" -C "${PACKAGE_PATH}" .
 
