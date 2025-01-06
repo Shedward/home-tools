079aa5520cd32505ec92017878169de966020c6b
Vlad Maltsev <shedwardx@gmail.com>
2025-01-06 14:17:52 +0300

    Removed unnecessary vpn files.

--- Sources/HomeTools/Leafs/VpnLeaf.swift
+++
@@ -1,7 +0,0 @@
-struct VpnLeaf: Leaf {
-  static let template = "vpn"
-
-  let current: VpnDeviceFull?
-  let availableStates: [VpnDevice.State]
-  let allDevices: [VpnDeviceFull]
-}
--- Sources/HomeTools/Migrations/CurrentVersion.swift
+++ Sources/HomeTools/Migrations/CurrentVersion.swift
@@ -1,2 +1,2 @@
 
-let currentVersion = "0.0.4.3"
+let currentVersion = "0.0.6.0"
