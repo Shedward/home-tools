7e6b72e4205bbe6ae3ad67d4427c5573378e9ef1
Vlad Maltsev <shedwardx@gmail.com>
2025-01-06 16:38:44 +0300

    More logs

--- Sources/HomeTools/configure.swift
+++ Sources/HomeTools/configure.swift
@@ -11,8 +11,12 @@ public func configure(_ app: Application) async throws {
         throw InternalError("Config path not found in environment at HOME_TOOLS_CONFIG_PATH")
     }
 
+    app.logger.info("Reading configuration from \(configPath)")
+
     let data = try Data(contentsOf: URL(fileURLWithPath: configPath))
     let configs = try JSONDecoder().decode(Configs.self, from: data)
+
+    app.logger.info("Connecting db from \(configs.dbFile)")
 
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.databases.use(.sqlite(.file(configs.dbFile)), as: .sqlite)
@@ -22,16 +26,22 @@ public func configure(_ app: Application) async throws {
 
     app.views.use(.leaf)
 
+    app.logger.info("Register migrations")
+
     // register migrations
     for migration in migrations() {
         app.migrations.add(migration)
     }
+
+    app.logger.info("Register routes")
 
     // register routes
     for toolController in toolControllers(app) {
         await services.toolsList.register(toolController)
         try app.register(collection: toolController)
     }
+
+    app.logger.info("Register tools")
 
     // static services
     for tool in tools() {
