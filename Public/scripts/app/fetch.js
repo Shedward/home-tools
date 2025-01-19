aa1401e1a4f7242c8a06fb89068a818634a8a67b
Vlad Maltsev <shedwardx@gmail.com>
2025-01-20 00:39:46 +0300

    Fixed messed up "all" option in online table

--- Public/scripts/app/fetch.js
+++ Public/scripts/app/fetch.js
@@ -1,40 +1,39 @@
-
 async function validate_response(response) {
-  if (response.status != 200) {
-    const body = await response.json();
-    const bodyString = JSON.stringify(body);
-    alert(`Request failed:\n${bodyString}`)
-  }
-} 
+    if (response.status != 200) {
+        const body = await response.json();
+        const bodyString = JSON.stringify(body);
+        alert(`Request failed:\n${bodyString}`);
+    }
+}
 
 async function a_fetch(path, params) {
-  let url = path;
+    let url = path;
 
-  // Convert query
-  if (params.query) {
-    url += "?" + new URLSearchParams(params.query);
-  }
+    // Convert query
+    if (params.query) {
+        url += "?" + new URLSearchParams(params.query);
+    }
 
-  var fetchParams = params;
+    var fetchParams = params;
 
-  // Convert headers
-  const defaultHeaders = { 
-    "Content-Type": "application/json" 
-  };
-  if (params.headers) {
-    fetchParams.headers = Object.assign(defaultHeaders, params.headers);
-  } else {
-    fetchParams.headers = defaultHeaders;
-  }
+    // Convert headers
+    const defaultHeaders = {
+        "Content-Type": "application/json",
+    };
+    if (params.headers) {
+        fetchParams.headers = Object.assign(defaultHeaders, params.headers);
+    } else {
+        fetchParams.headers = defaultHeaders;
+    }
 
-  // Convert body
-  if (params.body) {
-    fetchParams.body = JSON.stringify(params.body);
-  }
+    // Convert body
+    if (params.body) {
+        fetchParams.body = JSON.stringify(params.body);
+    }
 
-  let response = await fetch(url, fetchParams);
-  await validate_response(response);
-  return response;
-};
+    let response = await fetch(url, fetchParams);
+    await validate_response(response);
+    return response;
+}
 
 export { a_fetch };
--- Public/scripts/app/tools.js
+++ Public/scripts/app/tools.js
@@ -1,27 +1,26 @@
-
 function a_element_selector(type, id) {
-  return () => {
-    const formElem = document.getElementById(id);
+    return () => {
+        const formElem = document.getElementById(id);
 
-    if (!formElem) {
-      throw new Error(`${type} ${id} not found`);
-    }
+        if (!formElem) {
+            throw new Error(`${type} ${id} not found`);
+        }
 
-    return formElem
-  }
+        return formElem;
+    };
 }
 
 function a_remove_node(node) {
-  node.parentNode.removeChild(node)
-};
+    node.parentNode.removeChild(node);
+}
 
 function a_clean_node(node) {
-  node.textContent = '';
-};
+    node.textContent = "";
+}
 
 function a_css_query(strings, ...values) {
-  const escapedValues = values.map(CSS.escape)
-  return String.raw({ raw: strings }, ...escapedValues);
+    const escapedValues = values.map(CSS.escape);
+    return String.raw({ raw: strings }, ...escapedValues);
 }
 
 export { a_element_selector, a_remove_node, a_clean_node, a_css_query };
